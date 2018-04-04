ebc1aff80e884607534d5f104c5b6b6f4a2c6a24
Saevar2000 <saevar2000@gmail.com>
2018-04-04 15:19:46 +0000

    firebase auth settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBy0JDxkeYoWQaZwWJsniWyEJQDgNwXUyg",
+    authDomain: "bitar-1337.firebaseapp.com",
+    databaseURL: "https://bitar-1337.firebaseio.com",
+    projectId: "bitar-1337",
+    storageBucket: "bitar-1337.appspot.com",
+    messagingSenderId: "630607849378"
+  }
 };
